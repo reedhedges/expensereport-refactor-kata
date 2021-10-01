719c1356cb756d1b82463d6e499d730dfcca1774
Gregor Riegler <gregor.riegler@ebcont.com>
2021-10-01 12:49:16 +0200

    characterize CAR_RENTAL 500

--- expensereport-java/src/test/java/com/nelkinda/training/ExpenseReportShould.java
+++ expensereport-java/src/test/java/com/nelkinda/training/ExpenseReportShould.java
@@ -10,17 +10,40 @@ import java.util.Date;
 import java.util.List;
 
 @UseReporter(IdeaReporter.class)
-class ExpenseReportShould {
+class ExpenseReportCharacter {
     @Test
-    void name() {
-        ByteArrayOutputStream outputStream = new ApprovalUtilities().writeSystemOutToStringBuffer();
+    void noExpenses() {
+        ByteArrayOutputStream output = printExpenses(List.of());
+        Approvals.verify(output);
+    }
+
+    @Test
+    void carRental_500() {
+        ByteArrayOutputStream output = printSingleExpense(ExpenseType.CAR_RENTAL, 500);
+        Approvals.verify(output);
+    }
+
+    private ByteArrayOutputStream printSingleExpense(ExpenseType carRental, int amount) {
+        Expense expense = createExpense(carRental, amount);
+        return printExpenses(List.of(expense));
+    }
+
+    private Expense createExpense(ExpenseType type, int amount) {
+        Expense expense = new Expense();
+        expense.type = type;
+        expense.amount = amount;
+        return expense;
+    }
+
+    private ByteArrayOutputStream printExpenses(List<Expense> expenses) {
+        ByteArrayOutputStream output = new ApprovalUtilities().writeSystemOutToStringBuffer();
         ExpenseReport report = new ExpenseReport() {
             @Override
             protected Date now() {
                 return new Date(0);
             }
         };
-        report.printReport(List.of());
-        Approvals.verify(outputStream);
+        report.printReport(expenses);
+        return output;
     }
 }
