da8a2fe96c26f8432c551632f73c8b0a6054cb8d
Gregor Riegler <gregor.riegler@ebcont.com>
2021-10-02 21:26:15 +0200

    simplify tests

--- expensereport-java/src/test/java/com/nelkinda/training/ExpenseReportCharacter.java
+++ expensereport-java/src/test/java/com/nelkinda/training/ExpenseReportCharacter.java
@@ -2,24 +2,15 @@ package com.nelkinda.training;
 
 import org.approvaltests.ApprovalUtilities;
 import org.approvaltests.Approvals;
-import org.approvaltests.combinations.CombinationApprovals;
 import org.approvaltests.reporters.UseReporter;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.util.Date;
-import java.util.Map;
-import java.util.function.IntFunction;
 
 @UseReporter(IdeaReporter.class)
 class ExpenseReportCharacter {
     
-    private Map<String, IntFunction<Expense>> factories = Map.of(
-        "DINNER", Expense::createDinnerExpense,
-        "BREAKFAST", Expense::createBreakfastExpense,
-        "CAR_RENTAL", Expense::createCarRentalExpense
-    );
-
     @Test
     void noExpenses() {
         ByteArrayOutputStream output = printExpenses(new Expenses());
@@ -27,27 +18,17 @@ class ExpenseReportCharacter {
     }
 
     @Test
-    void singleExpenses() {
-        CombinationApprovals.verifyAllCombinations(
-            this::printSingleExpense,
-            new String[]{"DINNER", "BREAKFAST", "CAR_RENTAL"},
-            new Integer[]{-1, 0, 500, 999, 1000, 1001, 4999, 5000, 5001, 10000}
-        );
-    }
-
-    @Test
     public void manyExpenses() {
         ByteArrayOutputStream output = printExpenses(new Expenses(
-            Expense.createCarRentalExpense(1000),
-            Expense.createBreakfastExpense(3000)
+            Expense.createCarRentalExpense(7),
+            Expense.createBreakfastExpense(0),
+            Expense.createBreakfastExpense(1),
+            Expense.createBreakfastExpense(1000),
+            Expense.createBreakfastExpense(1001),
+            Expense.createDinnerExpense(5000),
+            Expense.createDinnerExpense(5001)
         ));
         Approvals.verify(output);
-    }
-
-    private ByteArrayOutputStream printSingleExpense(String factory, int amount) {
-        IntFunction<Expense> expenseFactory = factories.get(factory);
-        Expense expense = expenseFactory.apply(amount);
-        return printExpenses(new Expenses(expense));
     }
 
     private ByteArrayOutputStream printExpenses(Expenses expenses) {
