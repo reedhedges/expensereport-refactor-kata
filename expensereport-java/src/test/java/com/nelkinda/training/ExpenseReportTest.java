17115f6e9d77da0c10fff3156aa314c35453e607
Shorty <patrick.scheibert@hotmail.com>
2021-10-08 11:26:44 +0200

    refactor tests

--- expensereport-java/src/test/java/com/nelkinda/training/ExpenseReportTest.java
+++ expensereport-java/src/test/java/com/nelkinda/training/ExpenseReportTest.java
@@ -7,15 +7,14 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.util.Collections;
 import java.util.Date;
-import java.util.List;
 
 public class ExpenseReportTest {
 
+    private final ByteArrayOutputStream outputStream = new ApprovalUtilities().writeSystemOutToStringBuffer();
+    private final ExpenseReport expenseReport = new ExpenseReport();
+
     @Test
     public void printEmptyReport() {
-        ByteArrayOutputStream outputStream = new ApprovalUtilities().writeSystemOutToStringBuffer();
-        ExpenseReport expenseReport = new ExpenseReport();
-
         expenseReport.printReport(Collections.emptyList(), new Date(0));
 
         Approvals.verify(outputStream.toString());
@@ -23,15 +22,15 @@ public class ExpenseReportTest {
 
     @Test
     public void printDinner() {
-        ByteArrayOutputStream outputStream = new ApprovalUtilities().writeSystemOutToStringBuffer();
-        ExpenseReport expenseReport = new ExpenseReport();
+        expenseReport.printReport(Collections.singletonList(createDinnerExpense()), new Date(0));
+
+        Approvals.verify(outputStream.toString());
+    }
+
+    private Expense createDinnerExpense() {
         Expense expense = new Expense();
         expense.type = ExpenseType.DINNER;
         expense.amount = 10;
-        List<Expense> expenses = Collections.singletonList(expense);
-
-        expenseReport.printReport(expenses, new Date(0));
-
-        Approvals.verify(outputStream.toString());
+        return expense;
     }
 }
