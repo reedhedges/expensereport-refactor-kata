321cf6caa6e1e7c9b192c7161158b3a559dcc353
Matthias Kuehne <matthias.kuehne@ebcont.com>
2021-10-08 11:55:52 +0200

    bla

--- expensereport-csharp/expensereport-ut/UnitTest1.cs
+++ expensereport-csharp/expensereport-ut/UnitTest1.cs
@@ -42,5 +42,70 @@ namespace Tests
 
             Approvals.Verify(report.Output.ToString());
         }
+
+        [Test]
+        public void ExpenseReport_PrintReport_EmptyList()
+        {
+            var report = new ExpenseReportFake();
+
+            report.PrintReport(new List<Expense>()
+                {});
+
+            Approvals.Verify(report.Output.ToString());
+        }
+
+/*
+        [Test]
+        public void ExpenseReport_PrintReport_NullList()
+        {
+            var report = new ExpenseReportFake();
+
+            report.PrintReport(null);
+
+            Approvals.Verify(report.Output.ToString());
+        }
+        */
+
+        [Test]
+        public void ExpenseReport_PrintReport_OverExpenseDinner()
+        {
+            var report = new ExpenseReportFake();
+
+            report.PrintReport(new List<Expense>()
+                {
+                    new Expense { amount = 5001, type = ExpenseType.DINNER }
+                });
+
+            Approvals.Verify(report.Output.ToString());
+        }
+
+        [Test]
+        public void ExpenseReport_PrintReport_OverExpenseBreakfast()
+        {
+            var report = new ExpenseReportFake();
+
+            report.PrintReport(new List<Expense>()
+                {
+                    new Expense { amount = 1001, type = ExpenseType.BREAKFAST }
+                });
+
+            Approvals.Verify(report.Output.ToString());
+        }
+
+        [Test]
+        public void ExpenseReport_PrintReport_DoNotOverExpenseCarRental()
+        {
+            var report = new ExpenseReportFake();
+
+            report.PrintReport(new List<Expense>()
+                {
+                    new Expense { amount = int.MaxValue, type = ExpenseType.CAR_RENTAL }
+                });
+
+            Approvals.Verify(report.Output.ToString());
+        }
+    }
+
+    }
     }
 }
