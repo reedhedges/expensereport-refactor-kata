0e2a97fe97e1ed047d13888c44298a473c78aacd
Shorty <patrick.scheibert@hotmail.com>
2021-10-08 11:12:42 +0200

    add dinner test

--- expensereport-java/src/test/java/com/nelkinda/training/ExpenseReportTest.java
+++ expensereport-java/src/test/java/com/nelkinda/training/ExpenseReportTest.java
@@ -1,4 +1,5 @@
-import com.nelkinda.training.ExpenseReport;
+package com.nelkinda.training;
+
 import org.approvaltests.ApprovalUtilities;
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
@@ -6,14 +7,30 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 public class ExpenseReportTest {
+
     @Test
     public void printEmptyReport() {
         ByteArrayOutputStream outputStream = new ApprovalUtilities().writeSystemOutToStringBuffer();
         ExpenseReport expenseReport = new ExpenseReport();
 
         expenseReport.printReport(Collections.emptyList(), new Date(0));
+
+        Approvals.verify(outputStream.toString());
+    }
+
+    @Test
+    public void printDinner() {
+        ByteArrayOutputStream outputStream = new ApprovalUtilities().writeSystemOutToStringBuffer();
+        ExpenseReport expenseReport = new ExpenseReport();
+        Expense expense = new Expense();
+        expense.type = ExpenseType.DINNER;
+        expense.amount = 10;
+        List<Expense> expenses = Collections.singletonList(expense);
+
+        expenseReport.printReport(expenses, new Date(0));
 
         Approvals.verify(outputStream.toString());
     }
