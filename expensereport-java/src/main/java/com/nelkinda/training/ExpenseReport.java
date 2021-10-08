1c3a560c0c18932be3d7d900b45afe0737329b3e
Shorty <patrick.scheibert@hotmail.com>
2021-10-08 11:57:40 +0200

    refactor switch statement to use expense name from enum

--- expensereport-java/src/main/java/com/nelkinda/training/ExpenseReport.java
+++ expensereport-java/src/main/java/com/nelkinda/training/ExpenseReport.java
@@ -4,12 +4,28 @@ import java.util.Date;
 import java.util.List;
 
 enum ExpenseType {
-    DINNER, BREAKFAST, CAR_RENTAL
+    DINNER("Dinner"),
+    BREAKFAST("Breakfast"),
+    CAR_RENTAL("Car Rental");
+
+    private final String expenseName;
+
+    ExpenseType(String expenseName) {
+        this.expenseName = expenseName;
+    }
+
+    public String getExpenseName() {
+        return expenseName;
+    }
 }
 
 class Expense {
     ExpenseType type;
     int amount;
+
+    String getExpenseName() {
+        return type.getExpenseName();
+    }
 }
 
 public class ExpenseReport {
@@ -28,23 +44,10 @@ public class ExpenseReport {
                 mealExpenses += expense.amount;
             }
 
-            String expenseName = "";
-            switch (expense.type) {
-            case DINNER:
-                expenseName = "Dinner";
-                break;
-                case BREAKFAST:
-                    expenseName = "Breakfast";
-                    break;
-                case CAR_RENTAL:
-                    expenseName = "Car Rental";
-                    break;
-            }
-
             String mealOverExpensesMarker = expense.type == ExpenseType.DINNER && expense.amount > 5000
                     || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000 ? "X" : " ";
 
-            System.out.println(expenseName + "\t" + expense.amount + "\t" + mealOverExpensesMarker);
+            System.out.println(expense.getExpenseName() + "\t" + expense.amount + "\t" + mealOverExpensesMarker);
 
             total += expense.amount;
         }
@@ -52,4 +55,5 @@ public class ExpenseReport {
         System.out.println("Meal expenses: " + mealExpenses);
         System.out.println("Total expenses: " + total);
     }
+
 }
