b9526f5a11e685e1a4ea7be54876b1c22324b9fa
Shorty <patrick.scheibert@hotmail.com>
2021-10-08 11:03:13 +0200

    add first test for empty report

--- expensereport-java/src/test/java/ExpenseReportTest.java
+++ expensereport-java/src/test/java/ExpenseReportTest.java
@@ -1,2 +1,20 @@
+import com.nelkinda.training.ExpenseReport;
+import org.approvaltests.ApprovalUtilities;
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Collections;
+import java.util.Date;
+
 public class ExpenseReportTest {
+    @Test
+    public void printEmptyReport() {
+        ByteArrayOutputStream outputStream = new ApprovalUtilities().writeSystemOutToStringBuffer();
+        ExpenseReport expenseReport = new ExpenseReport();
+
+        expenseReport.printReport(Collections.emptyList(), new Date(0));
+
+        Approvals.verify(outputStream.toString());
+    }
 }
