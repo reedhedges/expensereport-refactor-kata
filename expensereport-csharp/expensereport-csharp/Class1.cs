9a0ce045d44a037b5e78072660227215fd058216
moerwald <andreas.mewald@gmail.com>
2021-10-13 12:20:35 +0200

    Added expense formatter and ExpenseRepository classes

--- expensereport-csharp/expensereport-csharp/Class1.cs
+++ expensereport-csharp/expensereport-csharp/Class1.cs
@@ -8,18 +8,39 @@ namespace expensereport_csharp
     {
         public void PrintReport(List<Expense> expenses)
         {
-            WriteOutput($"Expenses {GetCurrentDate()}");
+            var expenseRepo = new ExpenseRepository(expenses);
+            PrintHeader();
 
-            foreach (Expense expense in expenses)
-            {
-                WriteOutput($"{expense.GetExpenseName()}\t{ expense.Amount }\t{ (expense.IsOverexpensed() ? "X" : " ")}");
-            }
+            PrintExpenseDetails(expenses);
 
             WriteOutput($"Meal expenses:{expenses.Sum(e => e.GetMealExpense())}");
-            WriteOutput($"Total expenses: {expenses.Sum(e => e.Amount)}");
+            WriteOutput($"Total expenses: {expenseRepo.SumAmount()}");
         }
+
+        private void PrintHeader() => WriteOutput($"Expenses {GetCurrentDate()}");
+
+        private void PrintExpenseDetails(List<Expense> expenses)
+            =>
+            expenses.ForEach(expense => WriteOutput(ExpenseFormatter.GetExpenseDetail(expense)));
 
         protected virtual void WriteOutput(string message) => Console.WriteLine(message);
         protected virtual string GetCurrentDate() => DateTime.Now.ToString();
+    }
+
+    public static class ExpenseFormatter
+    {
+        public static string GetExpenseDetail(Expense expense) 
+            => 
+            $"{expense.GetExpenseName()}\t{ expense.Amount }\t{ (expense.IsOverexpensed() ? "X" : " ")}";
+    }
+
+    public class ExpenseRepository
+    {
+        private readonly IReadOnlyList<Expense> _expenses;
+
+        public ExpenseRepository(IReadOnlyList<Expense> expenses) => _expenses = expenses;
+
+        public int SumAmount() => _expenses.Sum(e => e.Amount);
+
     }
 }
