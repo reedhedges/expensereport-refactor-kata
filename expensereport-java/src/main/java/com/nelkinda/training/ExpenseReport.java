be86738e26c7d3178edb15563aebc7bec6175d29
Gregor Riegler <gregor.riegler@ebcont.com>
2021-10-16 18:54:29 +0200

    Refactor: move behaviour to ExpenseType to make adding new Type easier

--- expensereport-java/src/main/java/com/nelkinda/training/ExpenseReport.java
+++ expensereport-java/src/main/java/com/nelkinda/training/ExpenseReport.java
@@ -4,7 +4,31 @@ import java.util.Date;
 import java.util.List;
 
 enum ExpenseType {
-    DINNER, BREAKFAST, CAR_RENTAL
+    DINNER("Dinner", true, 5000),
+    BREAKFAST("Breakfast", true, 1000),
+    CAR_RENTAL("Car Rental", false, Integer.MAX_VALUE);
+
+    private final String name;
+    private final boolean meal;
+    private final int limit;
+
+    ExpenseType(String name, boolean meal, int limit) {
+        this.name = name;
+        this.meal = meal;
+        this.limit = limit;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    boolean isMeal() {
+        return meal;
+    }
+
+    boolean isOverLimit(Expense expense) {
+        return expense.amount > limit;
+    }
 }
 
 class Expense {
@@ -24,33 +48,17 @@ public class ExpenseReport {
         System.out.println("Expenses " + date);
 
         for (Expense expense : expenses) {
-            if (expense.type == ExpenseType.DINNER || expense.type == ExpenseType.BREAKFAST) {
+            if (expense.type.isMeal()) {
                 mealExpenses += expense.amount;
             }
-
-            String expenseName = "";
-            switch (expense.type) {
-                case DINNER:
-                    expenseName = "Dinner";
-                    break;
-                case BREAKFAST:
-                    expenseName = "Breakfast";
-                    break;
-                case CAR_RENTAL:
-                    expenseName = "Car Rental";
-                    break;
-            }
-
-            String mealOverExpensesMarker =
-                expense.type == ExpenseType.DINNER && expense.amount > 5000 ||
-                expense.type == ExpenseType.BREAKFAST && expense.amount > 1000 ? "X" : " ";
-
+            String expenseName = expense.type.getName();
+            String mealOverExpensesMarker = expense.type.isOverLimit(expense) ? "X" : " ";
             System.out.println(expenseName + "\t" + expense.amount + "\t" + mealOverExpensesMarker);
-
             total += expense.amount;
         }
 
         System.out.println("Meal expenses: " + mealExpenses);
         System.out.println("Total expenses: " + total);
     }
+
 }
